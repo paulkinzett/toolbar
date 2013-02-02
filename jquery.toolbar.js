c1c9b74457c70f8d89b95eb9e42f7135eae3df7a
João Ribeiro <joaopedro.ribeiro@sapo.pt>
2013-02-02 02:25:19 +0000

    Added right and bottom to position.

--- jquery.toolbar.js
+++ jquery.toolbar.js
@@ -34,6 +34,7 @@ if ( typeof Object.create !== 'function' ) {
             self.options = $.extend( {}, $.fn.toolbar.options, options );
             self.toolbar = $('<div class="tool-container gradient" />')
             .addClass('tool-'+self.options.position)
+            .addClass('tool-rounded')
             .append('<div class="tool-items" />')
             .append('<div class="arrow" />')
             .appendTo('body')
@@ -86,19 +87,34 @@ if ( typeof Object.create !== 'function' ) {
         getCoordinates: function( position, adjustment) {
             var self = this; 
             self.coordinates = self.$elem.offset();
-            if(position == 'top') { 
+            
+            switch(self.options.position)
+            {
+            case 'top':
                 return coordinates = {
                     left: self.coordinates.left-(self.toolbar.width()/2)+(self.$elem.width()/2),
                     top: self.coordinates.top-self.$elem.height()-adjustment,
                 }
-            }
-
-            if(position == 'left') { 
+            	break;
+            case 'left':
                 return coordinates = {
                     left: self.coordinates.left-(self.toolbar.width()/2)-(self.$elem.width()/2)-adjustment,
                     top: self.coordinates.top-(self.toolbar.height()/2)+(self.$elem.height()/2),
                 }
-            }
+            	break;
+            case 'right':
+                return coordinates = {
+                    left: self.coordinates.left+(self.toolbar.width()/2)+(self.$elem.width()/3)+adjustment,
+                    top: self.coordinates.top-(self.toolbar.height()/2)+(self.$elem.height()/2),
+                }
+                break;
+            case 'bottom':
+                return coordinates = {
+                    left: self.coordinates.left-(self.toolbar.width()/2)+(self.$elem.width()/2),
+                    top: self.coordinates.top+self.$elem.height()+adjustment,
+                }
+                break;
+            } 
 
         },
 
@@ -111,14 +127,22 @@ if ( typeof Object.create !== 'function' ) {
             var animation = {
                 'opacity': 1,
             };
-
-            if(self.options.position == 'top') {
-                animation.top = '-=20';
-            }
-
-            if(self.options.position == 'left') {
-                animation.left = '-=20';
-            }            
+            
+            switch(self.options.position)
+            {
+            case 'top':
+            	animation.top = '-=20';
+            	break;
+            case 'left':
+            	animation.left = '-=20';
+            	break;
+            case 'right':
+            	animation.left = '+=20';
+                break;
+            case 'bottom':
+            	animation.top = '+=20';
+                break;
+            }    
 
             self.toolbar.show().animate(animation, 200 );
         },
@@ -130,13 +154,22 @@ if ( typeof Object.create !== 'function' ) {
                 'opacity': 0,
             };
 
-            if(self.options.position == 'top') {
-                animation.top = '+=20';
-            }
-
-            if(self.options.position == 'left') {
-                animation.left = '+=20';
-            }             
+            switch(self.options.position)
+            {
+            case 'top':
+            	animation.top = '+=20';
+            	break;
+            case 'left':
+            	animation.left = '+=20';
+            	break;
+            case 'right':
+            	animation.left = '-=20';
+                break;
+            case 'bottom':
+            	animation.top = '-=20';
+                break;
+            }     
+            
             self.toolbar.animate(animation, 200, function() {
                 self.toolbar.hide();
             } );
